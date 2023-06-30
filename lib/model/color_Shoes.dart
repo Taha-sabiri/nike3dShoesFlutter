32c47f91d13a2f0d13f9a05a58e3cc89b5a4e02c
Taha Sabiri <121357512+Taha-sabiri@users.noreply.github.com>
2023-07-01 02:24:54 +0330

    add glb

---
+++ lib/model/color_Shoes.dart
@@ -0,0 +1,35 @@
+class ColorsShoes {
+  final String shoesColor;
+
+  const ColorsShoes({required this.shoesColor});
+}
+
+List<ColorsShoes> colorshoes = [
+  ColorsShoes(
+    shoesColor: 'assets/air-force-1-07-mens-shoes-jBrhbr.png',
+  ),
+  ColorsShoes(
+    shoesColor: 'assets/air-force-1-07-shoes-rWtqPn.png',
+  ),
+  ColorsShoes(
+    shoesColor: 'assets/air-force-1-big-kids-shoes-M7mcL9.png',
+  ),
+  ColorsShoes(
+    shoesColor: 'assets/air-force-1-07-mens-shoes-jBrhbr.png',
+  ),
+  ColorsShoes(
+    shoesColor: 'assets/air-force-1-07-shoes-rWtqPn.png',
+  ),
+  ColorsShoes(
+    shoesColor: 'assets/air-force-1-big-kids-shoes-M7mcL9.png',
+  ),
+  ColorsShoes(
+    shoesColor: 'assets/air-force-1-07-mens-shoes-jBrhbr.png',
+  ),
+  ColorsShoes(
+    shoesColor: 'assets/air-force-1-07-shoes-rWtqPn.png',
+  ),
+  ColorsShoes(
+    shoesColor: 'assets/air-force-1-big-kids-shoes-M7mcL9.png',
+  ),
+];
---
+++ lib/model/size.dart
@@ -0,0 +1,14 @@
+class Size {
+  final String size;
+
+  const Size({required this.size});
+}
+
+List<Size> size = [
+  Size(size: '39'),
+  Size(size: '40'),
+  Size(size: '41'),
+  Size(size: '42'),
+  Size(size: '43'),
+  Size(size: '44'),
+];
